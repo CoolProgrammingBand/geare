7153c37e36d5fee44c8eb5e456bd4e5faed5e881
Kittenlover229 <artur.roos@mail.ru>
2022-11-13 00:28:41 +0200

    Handle key presses, cursor movements  and closing as events

--- geare/windowing/Window.hpp
+++ geare/windowing/Window.hpp
@@ -15,6 +15,17 @@ struct Window : utils::Singleton<Window> {
   entt::delegate<bool()> on_should_close{
       [](const void *_) -> bool { return true; }, this};
   bool is_alive = false;
+  double cursor_x;
+  double cursor_y;
+
+  using KeyPressCallback = void(int key, int scancode, int action, int mods);
+  using CursorMoveCallback = void(double x, double y);
+
+  entt::delegate<KeyPressCallback> on_key_press{
+      [](const void *_, int key, int scancode, int action, int mods) {}};
+
+  entt::delegate<CursorMoveCallback> on_cursor_move{
+      [](const void *_, double x, double y) {}};
 
   Window() {
     if (!glfwInit())
@@ -23,6 +34,10 @@ struct Window : utils::Singleton<Window> {
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
     window = glfwCreateWindow(640, 480, "a GLFW Window", NULL, NULL);
     glfwMakeContextCurrent(window);
+
+    glfwSetWindowUserPointer(window, (void *)this);
+    glfwSetKeyCallback(window, _glfw_key_callback);
+    glfwSetCursorPosCallback(window, _glfw_cursor_position_callback);
 
     is_alive = true;
   }
@@ -42,20 +57,36 @@ struct Window : utils::Singleton<Window> {
   }
 
   ~Window() {
+    if (window)
+      glfwDestroyWindow(window);
+
     // TODO: maybe don't call this if GLFW didn't initialize?
     glfwTerminate();
   }
 
-  void show() {
-    glfwShowWindow(window);
-  }
+  void show() { glfwShowWindow(window); }
 
-  void hide() {
-    glfwHideWindow(window);
-  }
+  void hide() { glfwHideWindow(window); }
 
 protected:
   GLFWwindow *window;
+
+  static auto _window_from_glfw(GLFWwindow *window) -> Window & {
+    return *(Window *)glfwGetWindowUserPointer(window);
+  }
+
+  static void _glfw_key_callback(GLFWwindow *window, int key, int scancode,
+                                 int action, int mods) {
+    _window_from_glfw(window).on_key_press(key, scancode, action, mods);
+  }
+
+  static void _glfw_cursor_position_callback(GLFWwindow *w, double x,
+                                             double y) {
+    auto &window = _window_from_glfw(w);
+    window.cursor_x = x;
+    window.cursor_y = y;
+    window.on_cursor_move(x, y);
+  }
 };
 
 } // namespace geare::windowing
--- main.cpp
+++ main.cpp
@@ -5,16 +5,27 @@
 using namespace geare::windowing;
 
 int main(void) {
-  auto& window = Window::instance();
+  auto &window = Window::instance();
 
   window.show();
 
   bool should_close = false;
-  window.on_should_close.connect([](const void* should_close_ptr){
-    std::cout << "Should close triggered." << std::endl;
-    *(bool*)should_close_ptr = true;
-    return true;
-  }, &should_close);
+
+  window.on_should_close.connect(
+      [](const void *should_close_ptr) {
+        std::cout << "Should close triggered." << std::endl;
+        *(bool *)should_close_ptr = true;
+        return true;
+      },
+      &should_close);
+
+  window.on_key_press.connect([](const void *_, int key, int scancode,
+                                 int action,
+                                 int mods) { std::cout << key << std::endl; });
+
+  window.on_cursor_move.connect([](const void *_, double x, double y) {
+    std::cout << x << ' ' << y << std::endl;
+  });
 
   while (!should_close) {
     window.tick();
