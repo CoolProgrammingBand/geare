48ca2a71d482aca65a0bd7b0b1213b39a8eacd7d
Kittenlover229 <artur.roos@mail.ru>
2022-11-12 21:40:40 +0200

    Setup EnTT and GLM submodules and headers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "./ext/entt.hpp"
+#include "./ext/glm.hpp"
+
+struct Spatial {
+    glm::vec3 position;
+};
+
 int main(void) {
-  printf("Hello, world!");
+  entt::registry registry;
+
+  auto entity = registry.create();
+  registry.emplace<Spatial>(entity, Spatial());
+  std::cout << registry.get<Spatial>(entity).position.x << std::endl;
+  registry.destroy(entity);
+
   return 0;
 }
