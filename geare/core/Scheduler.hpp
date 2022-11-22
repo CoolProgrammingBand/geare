3af63fc923e5ce2c38c702874584999b3ade13e5
Kittenlover229 <artur.roos@mail.ru>
2022-11-22 23:11:06 +0200

    Fix segfault when using `alloca`

--- geare/core/Scheduler.hpp
+++ geare/core/Scheduler.hpp
@@ -1,7 +1,8 @@
 #ifndef _INCLUDE__GEARE__CORE__SCHEDULER_
 #define _INCLUDE__GEARE__CORE__SCHEDULER_
 
-#include "System.hpp"
+#include "../utils/Arena.hpp"
+#include "./System.hpp"
 #include <algorithm>
 #include <vector>
 
@@ -9,7 +10,6 @@ namespace geare::core {
 
 struct Scheduler {
   void add_system(System *system) {
-    // TODO: more efficient push
     systems.push_back(system);
     std::push_heap(
         systems.begin(), systems.end(), [](const System *a, const System *b) {
@@ -19,9 +19,8 @@ struct Scheduler {
   }
 
   virtual void tick(entt::registry &registry) {
-    auto *view = (std::byte *)alloca(max_view_size);
     for (auto &system : systems) {
-
+      auto view = tick_arena.allocate_raw(system->view_size);
       auto &contract = system->contract;
 
       if (contract.captured_component_count > 0) {
@@ -31,6 +30,8 @@ struct Scheduler {
         system->tick();
       }
     }
+
+    tick_arena.clear();
   }
 
   ~Scheduler() {
@@ -39,6 +40,7 @@ struct Scheduler {
   }
 
 protected:
+  utils::Arena<> tick_arena;
   std::size_t max_view_size = 0;
   std::vector<System *> systems;
 };
